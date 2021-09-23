731e260f200fc25910498885982abb624ddb8f56
Kevin Anthoney <kevin@kanthoney.co.uk>
2021-09-23 13:21:49 +0000

    Add spec for last commit

--- spec/fill-spec.js
+++ spec/fill-spec.js
@@ -16,7 +16,7 @@ describe('fill tests', () => {
         expect(/^[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/i.test(recordSet[0].order_id)).toBeTruthy();
       });
 
-      it('should not fill in full record', () => {
+      it('should not fill in full record', done => {
         const record = {
           company: "AAD010",
           order_id: "df4441f0-e41a-4ea8-8217-00c9c2983fe2",
@@ -52,27 +52,35 @@ describe('fill tests', () => {
           '"address":{"company":"Future Technologies Limited","street":"50 Whitworth Close","locality":"","city":"Manchester","region":"","postalCode":"M1 10WB","country":"GB"}},' +
           '"invoice":{"name":"Mr Francis Underwood","address":{"company":"Kingsbury PLC","street":"49 Baker Close","locality":"","city":"London","region":"","postalCode":"N13 1NC","country":"GB"}}}]'
         );
+        t.fillAsync(record).then(result => {
+          expect(JSON.stringify(result)).toBe(
+            '[{"company":"AAD010","order_id":"df4441f0-e41a-4ea8-8217-00c9c2983fe2","order_date":"2020-04-18","customer":"JNX79","delivery":{"name":"Mr Harry White",' +
+              '"address":{"company":"Future Technologies Limited","street":"50 Whitworth Close","locality":"","city":"Manchester","region":"","postalCode":' +
+              '"M1 10WB","country":"GB"}},"invoice":{"name":"Mr Francis Underwood","address":{"company":"Kingsbury PLC","street":"49 Baker Close","locality":"",' +
+              '"city":"London","region":"","postalCode":"N13 1NC","country":"GB"}}}]'
+          );
+        }).catch(fail).finally(done);
       });
-
+      
     });
-
+    
   });
-
+  
   describe('join tests', () => {
-
+    
     describe('orders tests', () => {
-
+      
       const j = joins.orders;
-
+      
       it('should fill empty record', () => {
         const result = j.fill({});
         const order_id = result.toJSON()[0].order_id;
         expect(JSON.stringify(result)).toBe(
           `[{"order_id":"${order_id}","delivery":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
-          '"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}}}]'
+             `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}}}]`
         );
       });
-
+        
       it('should fill empty record', () => {
         const record = {
           lines: [
@@ -94,11 +102,12 @@ describe('fill tests', () => {
         const order_id = result.toJSON()[0].order_id;
         expect(JSON.stringify(result)).toBe(
           `[{"order_id":"${order_id}","delivery":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
-          `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},"lines":[{"order_id":"${order_id}",` +
-          `"line_no":1,"sku":"AA451","description":"Hammer","qty":5},{"order_id":"${order_id}","line_no":2,"sku":"GN878","description":"Screwdriver","qty":8}]}]`
+             `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
+             `"lines":[{"order_id":"${order_id}","line_no":1,"sku":"AA451","description":"Hammer","qty":5},` +
+                       `{"order_id":"${order_id}","line_no":2,"sku":"GN878","description":"Screwdriver","qty":8}]}]`
         );
       });
-
+      
       it('should fill empty records', () => {
         const records = [
           {
@@ -144,16 +153,45 @@ describe('fill tests', () => {
         const order_ids = result.toJSON().map(record => record.order_id);
         expect(JSON.stringify(result)).toBe(
           `[{"order_id":"${order_ids[0]}","delivery":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
-          `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},"lines":[{"order_id":"${order_ids[0]}",` +
-          `"line_no":1,"sku":"AA451","description":"Hammer","qty":5},{"order_id":"${order_ids[0]}","line_no":2,"sku":"GN878","description":"Screwdriver","qty":8}]},` +
-          `{"order_id":"${order_ids[1]}","delivery":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
-          `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
-          `"lines":[{"order_id":"${order_ids[1]}","line_no":1,"sku":"VB989","description":"Pliers","qty":1},` +
-          `{"order_id":"${order_ids[1]}","line_no":2,"sku":"WS565","description":"Wire cutters","qty":1},` +
-          `{"order_id":"${order_ids[1]}","line_no":3,"sku":"SC674","description":"Hacksaw","qty":2}]}]`
+             `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` + 
+             `"lines":[{"order_id":"${order_ids[0]}","line_no":1,"sku":"AA451","description":"Hammer","qty":5},` +
+                       `{"order_id":"${order_ids[0]}","line_no":2,"sku":"GN878","description":"Screwdriver","qty":8}]},` +
+            `{"order_id":"${order_ids[1]}","delivery":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
+              `"invoice":{"name":"","address":{"company":"","street":"","locality":"","city":"","region":"","postalCode":"","country":"GB"}},` +
+              `"lines":[{"order_id":"${order_ids[1]}","line_no":1,"sku":"VB989","description":"Pliers","qty":1},` +
+                        `{"order_id":"${order_ids[1]}","line_no":2,"sku":"WS565","description":"Wire cutters","qty":1},` +
+                        `{"order_id":"${order_ids[1]}","line_no":3,"sku":"SC674","description":"Hacksaw","qty":2}]}]`
         );
       });
+      
+    });
 
+  });
+
+  describe('selectors', () => {
+
+    const { Table } = require('../index');
+
+    const t = new Table({
+      name: 'a',
+      columns: [
+        { name: 'a', notNull: true, primaryKey: true, default: () => 'a' },
+        { name: 'b', notNull: true, default: () => 'b' },
+        { name: 'c', notNull: true, default: () => 'c' }
+      ]
+    });
+
+    fit('should fill in primary key', done => {
+      const selector = col => col.primaryKey;
+      expect(JSON.stringify(t.fill({ c: 'c1' }, { selector }))).toBe(
+        '[{"a":"a","c":"c1"}]'
+      );
+      t.fillAsync({ c: 'c1' }, { selector }).then(result => {
+        expect(JSON.stringify(result)).toBe(
+          '[{"a":"a","c":"c1"}]'
+        );
+      }).catch(fail).finally(done);
+      done();
     });
 
   });
